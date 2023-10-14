fb8245e03b0b855bff3e4f4a009823d9d8b19d46
Denis <denis_kulikov_a_2003@mail.ru>
2023-10-14 23:44:42 +0700

    Add fragment & vertex shaders

--- src/OpenGL/main.cpp
+++ src/OpenGL/main.cpp
@@ -18,31 +18,29 @@
     Tutorial 04 - shaders
 */
 
+#include <iostream>
 #include <stdio.h>
 #include <string.h>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include "math_3d.h"
 
+
 GLuint VBO;
 
 
-static const char* pVS = "#version 320                                        \n\
-                                                                              \n\
-layout (location = 0) in vec3 Position;                                       \n\
+static const GLchar* pVS = "#version 110                                      \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    gl_Position = vec4(0.5 * Position.x, 0.5 * Position.y, Position.z, 1.0);  \n\
+    gl_Position = gl_Vertex;  \n\
 }";
 
-static const char* pFS = "#version 320                                        \n\
-                                                                              \n\
-out vec4 FragColor;                                                           \n\
+static const GLchar* pFS = "#version 110                                      \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    FragColor = vec4(1.0, 0.0, 0.0, 1.0);                                     \n\
+    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);                                  \n\
 }";
 
 static void RenderSceneCB()
@@ -78,65 +76,42 @@ static void CreateVertexBuffer()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 }
 
-static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+GLuint LoadShader(const GLchar** shader_code, GLuint type)
 {
-    GLuint ShaderObj = glCreateShader(ShaderType);
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, shader_code, NULL);
+    glCompileShader(shader);
 
-    if (ShaderObj == 0) {
-        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
-        exit(0);
+    GLint ok;
+    GLchar log[2000];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &ok);
+    if (!ok) {
+        glGetShaderInfoLog(shader, 2000, NULL, log);
+        printf("Shader: %s\n", log);
     }
 
-    const GLchar* p[1];
-    p[0] = pShaderText;
-    GLint Lengths[1];
-    Lengths[0]= strlen(pShaderText);
-    glShaderSource(ShaderObj, 1, p, Lengths);
-    glCompileShader(ShaderObj);
-    GLint success;
-    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        GLchar InfoLog[1024];
-        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
-        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
-        exit(1);
-    }
-
-    glAttachShader(ShaderProgram, ShaderObj);
+    return shader;
 }
 
 static void CompileShaders()
 {
-    GLuint ShaderProgram = glCreateProgram();
+    GLuint shader_color = LoadShader(&pFS, GL_FRAGMENT_SHADER);
+    GLuint shader_position = LoadShader(&pVS, GL_VERTEX_SHADER);
 
-    if (ShaderProgram == 0) {
-        fprintf(stderr, "Error creating shader program\n");
-        exit(1);
+    GLuint prog = glCreateProgram();
+    glAttachShader(prog, shader_color);
+    glAttachShader(prog, shader_position);
+    glLinkProgram(prog);
+
+    GLint ok;
+    GLchar log[2000];
+    glGetShaderiv(prog, GL_COMPILE_STATUS, &ok);
+    if (!ok) {
+        glGetProgramInfoLog(shader_color, 2000, NULL, log);
+        std::cout << log << std::endl;
     }
 
-    AddShader(ShaderProgram, pVS, GL_VERTEX_SHADER);
-    AddShader(ShaderProgram, pFS, GL_FRAGMENT_SHADER);
-
-    GLint Success = 0;
-    GLchar ErrorLog[1024] = { 0 };
-
-    glLinkProgram(ShaderProgram);
-    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
-	if (Success == 0) {
-		glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
-		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
-        exit(1);
-	}
-
-    glValidateProgram(ShaderProgram);
-    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
-    if (!Success) {
-        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
-        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
-        exit(1);
-    }
-
-    glUseProgram(ShaderProgram);
+    glUseProgram(prog);
 }
 
 int main(int argc, char** argv)
@@ -149,7 +124,6 @@ int main(int argc, char** argv)
 
     InitializeGlutCallbacks();
 
-    // Must be done after glut is initialized!
     GLenum res = glewInit();
     if (res != GLEW_OK) {
       fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
