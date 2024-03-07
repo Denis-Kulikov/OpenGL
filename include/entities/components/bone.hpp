0bd771a4bacd904bf5cbf5ff2b4be64d92abf811
Denis <denis_kulikov_a_2003@mail.ru>
2024-03-08 00:24:54 +0700

    Fix createSkelet

--- include/entities/components/bone.hpp
+++ include/entities/components/bone.hpp
@@ -13,27 +13,24 @@ public:
 
     static const char BONE[];
     static const char NAME[];
-    std::string name;
 
-    void addChildBone(std::string _name, Bone *_parent, std::map<std::string, Sprite> *_sprites)
+    void addChildBone(pugi::xml_node &node, std::string _name, Bone *_parent, std::map<std::string, Sprite> *_sprites)
     {
         Bone *child = new Bone(_name, _parent);
-        auto it = _sprites->find(child->name); // Поиск спрайта по имени в map
+        auto it = _sprites->find(child->name); 
         if (it != _sprites->end()) {
-            child->component.sprite = &(it->second); // Присвоение указателя на спрайт, если найден
+            child->component.sprite = &(it->second); 
             std::cout << child->name << "\t" << child->parent->name << std::endl;
-            children.push_back(child);
+            _parent->children.push_back(child);
+            child->parseBone(node, child, _sprites);
         }
-        // children.push_back(child);
     };
 
     void parseBone(pugi::xml_node &node, Bone *_parent, std::map<std::string, Sprite> *_sprites) {
-        std::cout << __FUNCTION__  << std::endl;
         for (pugi::xml_node boneNode = node.child(Bone::BONE); boneNode; boneNode = boneNode.next_sibling(Bone::BONE)) {
             std::string boneName = boneNode.attribute(Bone::NAME).as_string();
-            // Bone newBone(boneName);
-            _parent->addChildBone(boneName, this, _sprites);
-            parseBone(boneNode, this, _sprites);
+            _parent->addChildBone(boneNode, boneName, _parent, _sprites);
+            // parseBone(boneNode, _parent, _sprites);
         }
     }
 
@@ -47,6 +44,7 @@ public:
 
         std::string full_path = "assets/entities/" + _path + "/models/skelet/" + _name + ".xml";
         pugi::xml_document doc;
+        pugi::xml_node node;
         pugi::xml_parse_result parse_result = doc.load_file(full_path.c_str());
 
         if (!parse_result) {
@@ -54,22 +52,21 @@ public:
             return;
         }
 
-        parseBone(doc, this, _sprites);
+        node = doc.child("skelet");
+        name = node.attribute(Bone::NAME).as_string();
+        std::cout << name << std::endl;
+
+        parseBone(node, this, _sprites);
     }
 
-    // void pushSprites()
-    // {
-        // sprites = _sprites;   
-    // }
 
-
+    std::string name;
     Bone *parent = nullptr;
     std::vector<Bone*> children;
     Components component;
     // std::map<std::string, Sprite> *sprites;
     // std::vector<Components> animation;
 };
-    // int zIndex;
     // objectTransform _anchorPoint
 
 
